f8e00e90f5323154bca5b47ec67ccfafbae1e6a4
Neelkanth Damisetti <neelkanthdamisetti@gmail.com>
2026-06-23 15:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (97.43%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -5,9 +5,11 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         queue<pair<int,int>> q;
-        for(int i=0;i<grid.size();i++) {
-            for(int j=0;j<grid[i].size();j++) {
+        int fresh = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
                 if(grid[i][j] == 2) q.push({i,j});
+                if(grid[i][j] == 1) fresh++;
             }
         }
         while(!q.empty()) {
@@ -20,27 +22,27 @@ public:
                 if((h + 1 < n) && (grid[h + 1][v] == 1)) {
                     grid[h + 1][v] = 2;
                     q.push({h+1,v});
+                    fresh--;
                 }
                 if((h - 1 >= 0) && (grid[h - 1][v] == 1)) {
                     grid[h - 1][v] = 2;
                     q.push({h-1,v});
+                    fresh--;
                 }
                 if(((v + 1) < m) && (grid[h][v + 1] == 1)) {
                     grid[h][v + 1] = 2;
                     q.push({h,v+1});
+                    fresh--;
                 }
                 if((v - 1 >= 0) && (grid[h][v - 1] == 1)) {
                     grid[h][v - 1] = 2;
                     q.push({h,v-1});
+                    fresh--;
                 }
             }
             ans++;
         }
-        for(int i=0;i<grid.size();i++) {
-            for(int j=0;j<grid[i].size();j++) {
-                if(grid[i][j] == 1) return -1;
-            }
-        }
+        if(fresh > 0) return -1; 
         return max(ans,0);
     }
 };
