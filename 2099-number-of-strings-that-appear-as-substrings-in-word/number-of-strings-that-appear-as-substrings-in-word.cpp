da80afa44f7a6fd2e776afb42a27e67803f2796f
Neelkanth Damisetti <neelkanthdamisetti@gmail.com>
2026-06-29 13:44:42 +0530

    Time: 4 ms (15.05%) | Memory: 11.7 MB (61.65%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        unordered_map<string,int> hash;
-        for(int i=0;i<word.size();i++) {
-            string temp = "";
-            for(int j=i;j<word.size();j++) {
-                temp = temp + word[j];
-                hash[temp]++;
-            }
-        }
-
         int ans = 0;
-        for(int i=0;i<patterns.size();i++) {
-            if(hash[patterns[i]] > 0) ans++;
+        for(string it : patterns) {
+            if(word.find(it) != string::npos) ans++;
         }
         
         return ans;
