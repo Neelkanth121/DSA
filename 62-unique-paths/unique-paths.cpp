ddff6195a41c8fb3b36707e7c3622c9558910cc6
Neelkanth Damisetti <neelkanthdamisetti@gmail.com>
2026-08-03 23:31:26 +0530

    Time: 2 ms (20.97%) | Memory: 9.4 MB (54.68%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n, vector<int> (m,-1));
-        return helper(0,0,m,n,dp);
-    }
+        vector<vector<int>> dp(m,vector<int>(n,0));
 
-    int helper(int row, int col, int m, int n, vector<vector<int>>& dp) {
-        if((row == n-1) && (col == m-1)) return 1;
-        if(dp[row][col] != -1) return dp[row][col];
-        int right = 0;
-        int down = 0;
-        if((row + 1 < n) && (col < m)) {
-            right = 1 * helper(row+1,col,m,n,dp);
+        for(int row=0;row<m;row++) {
+            for(int col=0;col<n;col++) {
+                if((row == 0) && (col == 0)) {
+                    dp[row][col] = 1;
+                    continue;
+                }
+
+                int left = 0;
+                int up = 0;
+
+                if(row > 0) left = dp[row - 1][col];
+                if(col > 0) up = dp[row][col - 1];
+
+                dp[row][col] = left + up;
+            }
         }
-        if((row < n) && (col + 1 < m)) {
-            down = 1 * helper(row,col+1,m,n,dp);
-        }
-        return dp[row][col] = right + down;
+
+        return dp[m-1][n-1];
     }
 };
