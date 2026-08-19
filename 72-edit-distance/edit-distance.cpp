6bb3e8e80b1ba44c91ae61986263c4d6a422384f
Neelkanth Damisetti <neelkanthdamisetti@gmail.com>
2026-08-19 19:16:26 +0530

    Time: 7 ms (60.71%) | Memory: 14.1 MB (22.43%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -3,23 +3,28 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return helper(n-1,m-1,word1,word2,dp);
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        return helper(n - 1, m - 1, word1, word2, dp);
     }
 
     int helper(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if (i < 0) return j + 1;
+        if (j < 0) return i + 1;
 
-        int same = INT_MAX;
-        if(s[i] == t[j]) same = helper(i-1,j-1,s,t,dp);
-        int insert = 1 + helper(i,j-1,s,t,dp);
-        int del = 1 + helper(i-1,j,s,t,dp);
-        int replace = 1 + helper(i-1,j-1,s,t,dp);
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        dp[i][j] =  min(min(same,insert),min(del,replace));
-        return dp[i][j];
+        if (s[i] == t[j]) {
+            return dp[i][j] = helper(i - 1, j - 1, s, t, dp);
+        }
+
+        int insert = 1 + helper(i, j - 1, s, t, dp);
+        int del = 1 + helper(i - 1, j, s, t, dp);
+        int replace = 1 + helper(i - 1, j - 1, s, t, dp);
+
+        return dp[i][j] = min({insert, del, replace});
     }
 };
