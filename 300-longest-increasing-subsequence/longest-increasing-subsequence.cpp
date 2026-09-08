63d0f4872ab69f446ee1bc6d5072442ef693d518
Neelkanth Damisetti <neelkanthdamisetti@gmail.com>
2026-09-08 14:48:20 +0530

    Time: 586 ms (10.37%) | Memory: 296.6 MB (13.78%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return helper(1,0,nums,dp);
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        return helper(0,-1,nums,dp);
     }
 
     int helper(int index, int previndex, vector<int>& nums, vector<vector<int>>& dp) {
-        if(index-1 >= nums.size()) return 0;
+        if(index == nums.size()) return 0;
 
-        if(dp[index][previndex] != -1) return dp[index][previndex];
+        if(dp[index][previndex + 1] != -1) return dp[index][previndex + 1];
 
         int pick = 0;
-        if((previndex == 0) || (nums[index - 1] > nums[previndex - 1])) pick = 1 + helper(index+1,index,nums,dp);
+        if((previndex == -1) || (nums[index] > nums[previndex])) pick = 1 + helper(index+1,index,nums,dp);
         int notpick = helper(index+1,previndex,nums,dp);
 
-        return dp[index][previndex] = max(pick,notpick);
+        return dp[index][previndex + 1] = max(pick,notpick);
     }
 };
